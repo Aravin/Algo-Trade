3faf5ab239d0bbcea24a6d51ed897c764ad0a82f
Aravind Appadurai <aravin.it@gmail.com>
2021-03-18 23:37:16 +0530

    Added 0Auth and TypeScript Support

---
+++ app/pages/_app.tsx
@@ -0,0 +1,13 @@
+import '../styles/globals.css'
+
+import React from 'react';
+import { UserProvider } from '@auth0/nextjs-auth0';
+import { AppProps } from 'next/app'
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <UserProvider>
+      <Component {...pageProps} />
+    </UserProvider>
+  );
+}
