4b79826f5f5346aa69bf9a534ccfad71a61d2fbb
Aravin <aravin.it@gmail.com>
2022-04-24 00:26:39 +0530

    Added new table to keep latest market sentiment

--- app/cron/src/db/index.ts
+++ app/cron/src/db/index.ts
@@ -1,4 +1,4 @@
-import { DynamoDBClient, PutItemCommand, PutItemCommandInput } from '@aws-sdk/client-dynamodb';
+import { DeleteItemCommand, DeleteItemCommandInput, DynamoDBClient, PutItemCommand, PutItemCommandInput } from '@aws-sdk/client-dynamodb';
 
 export const ddbClient = (() => {
     let instance: DynamoDBClient;
@@ -18,7 +18,7 @@ export const ddbClient = (() => {
             const params: PutItemCommandInput = {
                 TableName: "algo_trade_sentiment",
                 Item: {
-                    date_time: { S: new Date().toISOString() },
+                    date_time: { S: data.dateTime },
                     global: { S: data.globalSentiment },
                     local: { S: data.indiaSentiment },
                     volatility: { S: data.volatility },
@@ -26,6 +26,49 @@ export const ddbClient = (() => {
             };
             try {
                 return local.send(new PutItemCommand(params));
+            } catch (err: any) {
+                console.log(err.message);
+            }
+        },
+        update: (data: any) => {
+            const local = createInstance();
+
+            try {
+                // delete
+                const deleteInput: DeleteItemCommandInput = {
+                    TableName: 'algo_trade_sentiment_latest',
+                    Key: {
+                        latest: { S: "latest" },
+                    }
+                }
+                local.send(new DeleteItemCommand(deleteInput));
+
+                // insert
+                const params: PutItemCommandInput = {
+                    TableName: "algo_trade_sentiment_latest",
+                    Item: {
+                        date_time: { S: data.dateTime },
+                        global: { S: data.globalSentiment },
+                        local: { S: data.indiaSentiment },
+                        volatility: { S: data.volatility },
+                        latest: { S: "latest" },
+                    },
+                };
+
+                return local.send(new PutItemCommand(params));
+
+                // const params: UpdateItemCommandInput = {
+                //     TableName: 'algo_trade_sentiment_latest',
+                //     Key: { latest: { S: "latest" } },
+                //     UpdateExpression: 'set date_time = :a, global = :b, local = :c, volatility = :d',
+                //     ExpressionAttributeValues: {
+                //         ':a': data.dateTime,
+                //         ':b': data.global,
+                //         ':c': data.local,
+                //         ':d': data.volatility,
+                //     }
+                // }
+                // local.send(new UpdateItemCommand(params));
             } catch (err: any) {
                 console.log(err.message);
             }
--- app/cron/src/handler.ts
+++ app/cron/src/handler.ts
@@ -1,15 +1,17 @@
-import { APIGatewayEvent, Context } from 'aws-lambda';
 import { ddbClient } from './db';
 import { getGlobalMarket } from './globalMarket';
 import { getIndiaMarket } from './indianMarket';
 
-export const run = async (event: APIGatewayEvent, context: Context): Promise<void> => {
+export const run = async (event: any, context: any): Promise<void> => {
     try {
         const globalSentiment = await getGlobalMarket();
         const { sentiment: indiaSentiment, trend } = await getIndiaMarket();
+        const dateTime = new Date().toISOString();
 
         console.log(globalSentiment, indiaSentiment, trend.atr.action);
-        ddbClient.insert({ globalSentiment, indiaSentiment, volatility: trend.atr.action });
+
+        ddbClient.insert({ globalSentiment, indiaSentiment, volatility: trend.atr.action, dateTime });
+        ddbClient.update({ globalSentiment, indiaSentiment, volatility: trend.atr.action, dateTime });
     }
     catch (err: any) {
         console.log(err.message);
