add72720cb6b2d59199c95496ca008af62ed43c9
Aravin <4869265+Aravin@users.noreply.github.com>
2022-09-27 19:56:35 +0530

    Removed old next.js project

--- app/client/pages/api/protected.tsx
+++
@@ -1,7 +0,0 @@
-// pages/api/protected.js
-import { withApiAuthRequired, getSession, Session } from '@auth0/nextjs-auth0';
-
-export default withApiAuthRequired(async function myApiRoute(req, res) {
-  const { user } = getSession(req, res) as Session;
-  res.json({ protected: 'My Secret', id: user.sub });
-});
